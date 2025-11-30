0ed79399a98969fe1a696090620c7d15cfb06424
btrostle <bttrostle@gmail.com>
2025-11-30 10:29:16 -0700

    catch up, player render, a/d rotation

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
+    dt = 0
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # updaters
+        player.update(dt)
+
+        # rendering
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
